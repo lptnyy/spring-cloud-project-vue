ea27ba347de7013ba397bb24669f58a0b7e50cd8
lptnii@qq.com <lptnii@qq.com>
2020-12-04 18:04:18 +0800

    提交 代码

---
+++ src/view/pro/assembly/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <Row>
+    <Col span="5">
+      <Card>
+      </Card>
+    </Col>
+    <Col span="6" offset="1">
+      <Card>
+      </Card>
+    </Col>
+    <Col span="11" offset="1">
+      <Card>
+      </Card>
+    </Col>
+  </Row>
+</template>
+
+<script>
+import vuedraggable from 'vuedraggable'
+export default {
+  name: 'Assembly',
+  components: {
+    vuedraggable
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+</style>
